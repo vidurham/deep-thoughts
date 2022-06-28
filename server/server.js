99ecdde09ae83bcda2d461f4ab6f42c14b2b5d09
vidurham <vdurham@prideseals.com>
2022-06-27 23:11:36 -0400

    react added

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 // import Apollo
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
@@ -26,6 +27,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
   // integrate Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
 }
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
